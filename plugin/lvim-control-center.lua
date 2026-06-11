a8145273b95169b93ebd30d6bb4108b587805ac8
Biser Stoilov <biserstoilov@school-exams.com>
2026-06-11 21:08:27 +0300

    Remove the lazy.nvim plugin guard file

--- plugin/lvim-control-center.lua
+++
@@ -1,9 +0,0 @@
-if vim.fn.has("nvim-0.10.0") == 0 then
-	print("Lvim Control Center requires Neovim >= 0.10.0")
-	return
-end
-
-if vim.g.loaded_lvim_control_center then
-	return
-end
-vim.g.loaded_lvim_control_center = true
